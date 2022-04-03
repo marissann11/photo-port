28811f944d6752eeba1bdb445aa9fa78c12bdf28
marissann11 <wilhite.marissa@gmail.com>
2022-04-02 23:35:06 -0400

    Gallery tests and PhotoList component added

--- src/App.js
+++ src/App.js
@@ -29,8 +29,7 @@ function App() {
       ></Nav>
       <main>
         <div>
-          <Gallery></Gallery>
-          <About></About>
+          <Gallery currentCategory={currentCategory}></Gallery> <About></About>
         </div>
       </main>
     </div>
