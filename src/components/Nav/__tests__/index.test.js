52bd6ad00a8a81589d85f2afd3e20f1e8340ead2
marissann11 <wilhite.marissa@gmail.com>
2022-04-03 00:24:56 -0400

    added contact form conditional rendering

--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -3,38 +3,32 @@ import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Nav from '..';
 
-// const categories = [
-//   { name: 'portraits', description: 'Portraits of people in my life' },
-// ];
-// const mockCurrentCategory = jest.fn();
-// const mockSetCurrentCategory = jest.fn();
-
 afterEach(cleanup);
 
-describe('Nav component', () => {
-  // baseline test
+describe('Nav component renders', () => {
   it('renders', () => {
     render(<Nav />);
   });
 
-  // snapshot test
   it('matches snapshot', () => {
     const { asFragment } = render(<Nav />);
 
     expect(asFragment()).toMatchSnapshot();
   });
 });
+
 describe('emoji is visible', () => {
   it('inserts emoji into the h2', () => {
-    // Arrange
     const { getByLabelText } = render(<Nav />);
-    // Assert
+
     expect(getByLabelText('camera')).toHaveTextContent('📸');
   });
 });
+
 describe('links are visible', () => {
   it('inserts text into the links', () => {
     const { getByTestId } = render(<Nav />);
+
     expect(getByTestId('link')).toHaveTextContent('Oh Snap!');
     expect(getByTestId('about')).toHaveTextContent('About me');
   });
